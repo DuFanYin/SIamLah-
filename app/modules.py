c04b74a766ddf3fce5c7833badcf17ecc531e6f3
Hang Zhengyang <122686006h@gmail.com>
2024-08-30 14:22:56 +0800

    initial commit, create file structure

---
+++ app/modules.py
@@ -0,0 +1,26 @@
+# all service layer functions
+
+# helper function to read preset data
+# to outpur dict of zone coor
+def read_zone_coor(file):
+    pass
+
+
+# function 1 
+# input:  number_per_zone, a  dictionary {zone_id : number_in_the_zone}
+# output: a dictionary { zone_id : reletive_density}
+def generate_crowd_density(number_per_zone):
+    pass
+
+
+
+# function 2
+def generate_heat_map():
+    pass
+
+
+def generate_final_map():
+    pass
+
+
+
---
+++ app/test_data/readme.md
@@ -0,0 +1,9 @@
+contain test data to run the demonstration
+
+in actual implementation
+all these data will be inputed by the organiser
+
+content:
+    zone_coordinate
+    event_venue_map
+    radius
